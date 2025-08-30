f2a9d5e5f79ac4a6358e5c477e8cef657c1db114
Miguel RS Gonçalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-08-30 10:41:16 +0100

    Added accept dialog on CLF deletion

--- custom-global-light-function/addons/custom_global_light_function/src/nodes/delete_clf_button.gd
+++ custom-global-light-function/addons/custom_global_light_function/src/nodes/delete_clf_button.gd
@@ -2,9 +2,13 @@
 extends Button
 
 @export var CGLF: CGLF_Manager = null
+@export var _accept_dialog: AcceptDialog = null
 
 func _ready() -> void:
 	icon = EditorInterface.get_base_control().get_theme_icon("Remove", "EditorIcons")
 
 func _on_pressed() -> void:
+	_accept_dialog.popup()
+
+func _on_accept_dialog_confirmed() -> void:
 	CGLF.delete_light_function()
